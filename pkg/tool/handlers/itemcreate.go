4bc6a9b84227eb02e510348bac37e80a1078d009
Charlie Egan <charlieegan3@users.noreply.github.com>
2022-10-05 08:10:20 +0100

    Log item create errors to response

--- pkg/tool/handlers/itemcreate.go
+++ pkg/tool/handlers/itemcreate.go
@@ -12,24 +12,28 @@ import (
 func BuildItemCreateHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 	goquDB := goqu.New("postgres", db)
 
-	return func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
 
 		feed, ok := vars["feed"]
 		if !ok || feed == "" {
-			writer.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("feed var missing"))
 			return
 		}
 
-		if feedRegex.MatchString(feed) == false {
-			writer.WriteHeader(http.StatusBadRequest)
+		if !feedRegex.MatchString(feed) {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("feed didn't match regex"))
 			return
 		}
 
 		var items []toolAPIs.PayloadNewItem
-		err := json.NewDecoder(request.Body).Decode(&items)
+		err := json.NewDecoder(r.Body).Decode(&items)
 		if err != nil {
-			writer.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+
 			return
 		}
 
@@ -37,17 +41,20 @@ func BuildItemCreateHandler(db *sql.DB) func(http.ResponseWriter, *http.Request)
 
 		for _, item := range items {
 			if item.Title == "" {
-				writer.WriteHeader(http.StatusBadRequest)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("title can't be blank"))
 				return
 			}
 
 			if len(item.Title) > 500 {
-				writer.WriteHeader(http.StatusBadRequest)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("title too long"))
 				return
 			}
 
 			if len(item.Body) > 100000 {
-				writer.WriteHeader(http.StatusBadRequest)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("body too long"))
 				return
 			}
 
@@ -63,10 +70,10 @@ func BuildItemCreateHandler(db *sql.DB) func(http.ResponseWriter, *http.Request)
 
 		_, err = ins.Executor().Exec()
 		if err != nil {
-			writer.WriteHeader(http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		writer.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusOK)
 	}
 }
